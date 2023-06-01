0351a5c1d4b96c53eb5d2983322345c7cf9c4076
michaljanus <michaljanus012@gmail.com>
2023-06-01 09:16:29 +0200

    removed comments

--- YouTennis.Web/src/app/models/booking.ts
+++ YouTennis.Web/src/app/models/booking.ts
@@ -1,10 +1,6 @@
 import { Resource } from "./resource";
 
 export class Booking extends Resource{
-    // courtId: number;
-    // date: string;
-    // time: string;
-
     CourtId: string;
     BookingDateAsString: string;
     CourtAddress: string;
